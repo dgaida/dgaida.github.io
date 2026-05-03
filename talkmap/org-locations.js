b3fd43a8c33e802486f005cf70802428555b373e
GitHub Action <action@github.com>
2026-05-03 16:33:37 +0000

    Automated update of talk locations

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,8 +1,8 @@
 var addressPoints = [
   [
-    "Synergizing Language Models and Biogas Plant Control: A GPT-4 Approach<br />8th IWA World Conference on Anaerobic Digestion; Istanbul, Turkey",
-    41.006381,
-    28.9758715
+    "Natural-Language Robot Manipulation via MCP: An Integrated Framework for Vision-Guided Pick-and-Place Automation<br />9th Machine Learning for Cyber Physical Systems (ML4CPS) Conference; Berlin, Germany",
+    52.5173885,
+    13.3951309
   ],
   [
     "Entwicklung intelligenter autonomer Assistenten<br />Konrad-Adenauer-Stiftung: 22. Fachschaftstreffen MINT - Zukunft gestalten durch KI; Dresden, Germany",
@@ -10,8 +10,8 @@ var addressPoints = [
     13.7381437
   ],
   [
-    "Natural-Language Robot Manipulation via MCP: An Integrated Framework for Vision-Guided Pick-and-Place Automation<br />9th Machine Learning for Cyber Physical Systems (ML4CPS) Conference; Berlin, Germany",
-    52.5173885,
-    13.3951309
+    "Synergizing Language Models and Biogas Plant Control: A GPT-4 Approach<br />8th IWA World Conference on Anaerobic Digestion; Istanbul, Turkey",
+    41.006381,
+    28.9758715
   ]
 ];
